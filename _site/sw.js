219fc69a0427ba5191262c6f104431ce775a8728
mehmetcan.doslu <mehmetcan.doslu@tubitak.gov.tr>
2021-10-12 01:14:15 +0300

    edit config

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/_site/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211012.0552';
+const cacheName = 'chirpy-20211012.0112';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
